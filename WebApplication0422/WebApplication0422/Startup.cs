0c4243d35b1feea0cd141c1c701f3aa591599e1d
SissyTseng <sissytseng@gmail.cm>
2015-04-22 16:23:13 +0800

    v1

---
+++ WebApplication0422/WebApplication0422/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication0422.Startup))]
+namespace WebApplication0422
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
